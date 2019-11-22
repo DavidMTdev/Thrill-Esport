bae590ce2346d1a68f5a853024c8e7d3720bf242
David Teixeira <david.teixeira31@gmail.com>
2019-11-22 22:56:22 +0100

    add includes

---
+++ public/includes/footer.php
@@ -0,0 +1,34 @@
+<footer class="py-5 pt-md-5 border-top thrill-bg-blue">
+    <div class="row">
+        <div class="col-12 col-md text-center">
+            <h5 class="thrill-white">Menu</h5>
+            <ul class="list-unstyled text-small text-center">
+                <li><a class="thrill-light" href="#">A Propos</a></li>
+                <li><a class="thrill-light" href="#">Actualite</a></li>
+                <li><a class="thrill-light" href="#">Equipe</a></li>
+                <li><a class="thrill-light" href="#">Macths</a></li>
+                <li><a class="thrill-light" href="#">Palmares</a></li>
+                <li><a class="thrill-light" href="#">Sponsors</a></li>
+            </ul>
+        </div>
+        <?php if(false): ?>
+        <div class="col-12 col-md text-center">
+            <h5 class="thrill-white">Partenaires</h5>
+            <ul class="list-unstyled text-small">
+            </ul>
+        </div>
+        <?php endif; ?>
+        <div class="col-12 col-md text-center">
+            <h5 class="thrill-white">A Propos</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="thrill-light" href="#">Objectif</a></li>
+                <li><a class="thrill-light" href="#">Contact</a></li>
+                <li><a class="thrill-light" href="#">Terms</a></li>
+            </ul>
+        </div>
+        <div class="col-12 col-md text-center">
+            <img class="mb-2" src="/asset/img/logo/Logo_transparence_v2.png" alt="" width="128" height="128">
+            <small class="d-block mb-3 thrill-light">&copy; 2017-2019</small>
+        </div>
+    </div>
+</footer>
