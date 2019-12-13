4624b544ee27add6f15c7540fbaab3ed74c7ed9d
David Teixeira <david.teixeira31@gmail.com>
2019-12-13 18:11:03 +0100

    rename file

---
+++ dev/development.php
@@ -0,0 +1,32 @@
+   <?php require_once("../includes/header.php") ?>
+
+   <div class="thrill-background">
+   </div>
+   <div class="thrill-container">
+       <h1 class="thrill-text-shadow">Actualite</h1>
+       <h2 class="thrill-text-shadow">En developpement...</h2>
+       <br>
+       <br>
+       <h2 class="thrill-text-shadow">Cette page sera disponible dans</h2>
+       <div class="thrill-count-container">
+           <div class="thrill-count">
+               <div class="thrill-countd">
+                   <span id="days">00</span>
+                   JOURS
+               </div>
+               <div class="thrill-countd">
+                   <span id="hours">00</span>
+                   HEURES
+               </div>
+               <div class="thrill-countd">
+                   <span id="minutes">00</span>
+                   MINUTES
+               </div>
+               <div class="thrill-countd">
+                   <span id="seconds">00</span>
+                   SECONDES
+               </div>
+           </div>
+       </div>
+   </div>
+   <?php require_once("../includes/footer.php") ?>
