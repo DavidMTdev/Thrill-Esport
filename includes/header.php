0ecb4322fb3362fbe97aaff8623e44ab297b8fa5
David Teixeira <david.teixeira31@gmail.com>
2019-12-13 18:16:12 +0100

    modify files

--- includes/header.php
+++ includes/header.php
@@ -1,38 +1,40 @@
-<header>
-    <nav class="navbar navbar-expand-lg fixed-top py-1 navbar-dark PaybAck thrill-bg-blue thrill-nav">
-        <div class="container d-flex flex-md-row justify-content-between">
-            <a class="navbar-brand thrill-white align-center" href="index.php">
-                <img src="/asset/img/logo/v2/Logo_transparence_v2.png" width="32" height="32" class="d-inline-block align-top" alt="Thrill Esport Culb Logo official">
-                Thrill Esport
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">Presentation</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="comingsoon.php">Actualite</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="comingsoon.php">Equipe</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Competitions
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="comingsoon.php">Palmares</a>
-                            <a class="dropdown-item" href="index.php">Resultats</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="comingsoon.php">Sponsors</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
+<?php require_once("function.php") ?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/asset/css/style.css">
+
+    <?php if ($_SERVER["REQUEST_URI"] === "/") : ?>
+        <link rel="stylesheet" href="/asset/css/home.css">
+        <title>Thrill Esport Club</title>
+    <?php elseif ($_SERVER["REQUEST_URI"] === "/about") : ?>
+        <link rel="stylesheet" href="/asset/css/about.css">
+        <title>La Présentation De La Structure</title>
+    <?php elseif ($_SERVER["REQUEST_URI"] === "/team/fortnite") : ?>
+        <link rel="stylesheet" href="/asset/css/team.css">
+        <title>Nos Equipe</title>
+
+    <?php elseif ($_SERVER["REQUEST_URI"] === "/dev/development") : ?>
+        <link rel="stylesheet" href="/asset/css/development.css">
+        <title>En Développement</title>
+    <?php endif; ?>
+
+</head>
+
+<body>
+
+    <header>
+        <?php require_once("nav.php") ?>
+    </header>
+    <main>
+
+        <!-- /* --------------------------------- logger --------------------------------- */ -->
+        <!-- <?= var_dump($_SERVER) ?> -->
