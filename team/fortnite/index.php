9c477e6fda45927b93a59e5fc61165695df3cd40
David Teixeira <david.teixeira31@gmail.com>
2019-12-15 17:25:08 +0100

    add page team fortnite

---
+++ team/fortnite/index.php
@@ -0,0 +1,237 @@
+<?php require_once("../../includes/header.php") ?>
+
+<section id="header">
+    <div class="jumbotron team-header fortnite">
+        <div class="shadow">
+            <div class="container">
+                <div class="d-flex justify-content-center logo-heading">
+                    <img class="game-logo " src="/asset/img/fortnite/logo/fortnite.png" alt="logo fortnite">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="position-relative">
+                        <h1 class="font thrill-white h5 mb-4">Notre equipe fortnite</h1>
+                        <div class="arrow">
+                            <a href="/team/fortnite/#team">
+                                <img src="/asset/img/icon/64/arrow-white.png" alt="flèche vers le bas">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.shadow -->
+    </div>
+    <!-- /.jumbotron -->
+</section>
+<section id="team" class="thrill-bg-white">
+    <div class="container py-5">
+        <h3 class="display-4 text-uppercase font-weight-bold thrill-blue">
+            roster fortnite
+        </h3>
+    </div>
+    <div class="container py-5">
+        <div class="row mb-5">
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Neziõ</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">CAYET Clément</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/ImNezio">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/cannonniere-de-la-brute-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Fish</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">PERROT William</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/SocreamD">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                        <a href="https://www.twitch.tv/thrill_fiish">
+                            <img src="/asset/img/icon/Twitch/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/peste-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Hermes</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">LANGEARD Tom</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/FB_Hermes">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/mecano-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        <div class="row mb-5">
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Djo</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">ALFONSO Jordan</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">capitaine</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/BlkDjo">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/jade-la-rouge-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Rehy</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">DE OLIVEIRA Estéban</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/DLVR78">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                        <a href="https://www.twitch.tv/rehyou78">
+                            <img src="/asset/img/icon/Twitch/white.png" alt="">
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCNTxzaNOI35uILOHNBBEC8w">
+                            <img src="/asset/img/icon/Youtube/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/soldat-laineux-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Chips</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">GRIGNY Paul</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/GrignyPaul">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/soldat-d-elite-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+        <div class="row mb-5">
+            <div class="col-4">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">Alpha</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">DELFIM Daniel</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/BlkHunterAlpha">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="">
+                        </a>
+                        <a href="https://www.twitch.tv/daanieldlm">
+                            <img src="/asset/img/icon/Twitch/white.png" alt="">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fortnite/skins/veinarde-2.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+    </div>
+</section>
+
+<?php require_once("../../includes/footer.php") ?>
