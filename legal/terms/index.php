e709de9d29edcd4666664b6ac3d7f33d39f291dc
David Teixeira <david.teixeira31@gmail.com>
2019-12-27 18:02:34 +0100

    add page terms

---
+++ legal/terms/index.php
@@ -0,0 +1,130 @@
+<?php require_once("../../includes/header.php") ?>
+
+<!-- /.section #main -->
+<section id="main" class="thrill-bg-white">
+    <div class="container py-5">
+        <div class="row mb-4">
+            <h2 class="thrill-blue">Mentions L'egales</h2>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">1. Présentation du site</h3>
+            <p class="text-justify">En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site thrillesportclub.fr l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
+            <dl class="row pl-5 my-4">
+                <dt class="col-sm-3 thrill-blue">Propriétaire :</dt>
+                <dd class="col-sm-9">Thrill eSport Club – Association de loi 1901 85230634900014– 65 rue de la Croix 92000, Nanterre</dd>
+
+                <dt class="col-sm-3 thrill-blue">Créateur :</dt>
+                <dd class="col-sm-9">
+                    <p>David Da Silva Teixeira</p>
+                </dd>
+
+                <dt class="col-sm-3 thrill-blue">Responsable publication :</dt>
+                <dd class="col-sm-9">
+                    Thrill eSport Club – <a href="mailto:contact@thrillesportclub.fr">contact@thrillesportclub.fr</a>
+                    <p>Le responsable publication est une personne physique ou une personne morale.</p>
+                </dd>
+
+                <dt class="col-sm-3 thrill-blue">Webmaster :</dt>
+                <dd class="col-sm-9">Thrill eSport Club – <a href="mailto:contact@thrillesportclub.fr">contact@thrillesportclub.fr</a></dd>
+
+                <dt class="col-sm-3 thrill-blue">Hébergeur :</dt>
+                <dd class="col-sm-9">1&1 IONUS</dd>
+            </dl>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">2. Conditions générales d’utilisation du site et des services proposés</h3>
+            <p class="text-justify">L’utilisation du site Thrill eSport Club implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site thrillesportclub.fr sont donc invités à les consulter de manière régulière.</p>
+            <p class="text-justify">Ce site est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par Thrill eSport Club, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.
+                Le site thrillesportclub.fr est mis à jour régulièrement par David Da Silva Teixeira. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.
+            </p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">3. Description des services fournis</h3>
+            <p class="text-justify">Le site thrillesportclub.fr g a pour objet de fournir une information concernant l’ensemble des activités de la société.</p>
+            <p class="text-justify pt-3">Thrill eSport Club s’efforce de fournir sur le site thrillesportclub.fr des informations aussi précises que possible. Toutefois, il ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.</p>
+            <p class="text-justify">Tous les informations indiquées sur le site thrillesportclub.fr sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site thrillesportclub.fr ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">4. Limitations contractuelles sur les données techniques</h3>
+            <p class="text-justify">Le site utilise la technologie JavaScript ainsi que PHP. <br> Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.</p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">5. Propriété intellectuelle et contrefaçons</h3>
+            <p class="text-justify">Thrill eSport Club est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes, sons, logiciels.</p>
+            <p class="text-justify">Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de : Thrill eSport Club.</p>
+            <p class="text-justify">Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">6. Limitations de responsabilité</h3>
+            <p class="text-justify">Thrill eSport Club ne pourra être tenue responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site gamersorigin.com, et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.</p>
+            <p class="text-justify">Thrill eSport Club ne pourra également être tenue responsable des dommages indirects (tels par exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du site thrillesportclub.fr.</p>
+            <p class="text-justify">Des espaces interactifs (possibilité de poser des questions dans l’espace contact) sont à la disposition des utilisateurs. Thrill eSport Club se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, Thrill eSport Club se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie…).</p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">7. Gestion des données personnelles</h3>
+            <p class="text-justify">En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.</p>
+            <p class="text-justify">A l’occasion de l’utilisation du site thrillesportclub.fr, peuvent être recueillies : l’URL des liens par l’intermédiaire desquels l’utilisateur a accédé au site thrillesportclub.fr, le fournisseur d’accès de l’utilisateur, l’adresse de protocole Internet (IP) de l’utilisateur.</p>
+            <p class="text-justify">En tout état de cause Thrill eSport Club ne collecte des informations personnelles relatives à l’utilisateur que pour le besoin de certains services proposés par le site thrillesportclub.fr. L’utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu’il procède par lui-même à leur saisie. Il est alors précisé à l’utilisateur du site thrillesportclub.fr l’obligation ou non de fournir ces informations.</p>
+            <p class="text-justify">Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en effectuant sa demande écrite et signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.</p>
+            <p class="text-justify">Aucune information personnelle de l’utilisateur du site thrillesportclub.fr n’est publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Seule l’hypothèse du rachat de Thrill eSport Club et de ses droits permettrait la transmission des dites informations à l’éventuel acquéreur qui serait à son tour tenu de la même obligation de conservation et de modification des données vis à vis de l’utilisateur du site thrillesportclub.fr.</p>
+            <p class="text-justify">Le site n’est pas déclaré à la CNIL car il ne recueille pas d’informations personnelles.
+                Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
+            </p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">8. Liens hypertextes</h3>
+            <p class="text-justify">Le site thrillesportclub.fr contient un certain nombre de liens hypertextes vers d’autres sites, mis en place avec l’autorisation de Thrill eSport Club. Cependant, Thrill eSport Club n’a pas la possibilité de vérifier le contenu des sites ainsi visités, et n’assumera en conséquence aucune responsabilité de ce fait.</p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">9. Droit applicable et attribution de juridiction</h3>
+            <p class="text-justify">Tout litige en relation avec l’utilisation du site thrillesportclub.fr est soumis au droit français. </p>
+            <p class="text-justify">Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris. </p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">10. Les principales lois concernéesn</h3>
+            <p class="text-justify">Loi n° 78-17 du 6 janvier 1978, notamment modifiée par la loi n° 2004-801 du 6 août 2004 relative à l’informatique, aux fichiers et aux libertés.</p>
+            <p class="text-justify">Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique.</p>
+        </div>
+        <!-- /.row -->
+
+        <div class="row thrill-gray mb-4">
+            <h3 class="h5 thrill-dark-blue font">11. Lexique</h3>
+            <dl class="row pl-5 my-4">
+                <dt class="col-sm-3 thrill-blue">Utilisateur :</dt>
+                <dd class="col-sm-9">Internaute se connectant, utilisant le site susnommé.</dd>
+
+                <dt class="col-sm-3 thrill-blue">Informations personnelles :</dt>
+                <dd class="col-sm-9">
+                    <p>« les informations qui permettent, sous quelque forme que ce soit, directement ou non, l’identification des personnes physiques auxquelles elles s’appliquent » </p>
+                </dd>
+            </dl>
+            <p class="text-justify">(article 4 de la loi n° 78-17 du 6 janvier 1978).</p>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
+<!-- /.section #article -->
+
+<?php require_once("../../includes/footer.php") ?>
