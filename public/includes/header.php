db8436cb803f45abd4138c1057c7cf8d22c108dc
David Teixeira <david.teixeira31@gmail.com>
2020-01-19 19:29:26 +0100

    add favicon

--- public/includes/header.php
+++ public/includes/header.php
@@ -22,6 +22,8 @@
         <meta name="twitter:creator" content="@Thrill_EC" />
     <?php endif; ?>
 
+    <link rel="icon" type="image/png" href="/asset/img/logo/v3/thrill-esport-logo.png" />
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/asset/css/style.css">
