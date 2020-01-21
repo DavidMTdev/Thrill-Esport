8cf76a45adf09dba005bd1438199ae080073a622
David Teixeira <david.teixeira31@gmail.com>
2020-01-21 21:33:32 +0100

    Add new player fifa

--- public/team/fifa/index.php
+++ public/team/fifa/index.php
@@ -107,9 +107,6 @@
                 </div>
             </div>
             <!-- /.col -->
-        </div>
-        <!-- /.row -->
-        <div class="row">
             <div class="col-md-4 mb-5">
                 <div class="card-player">
                     <div class="pseudo">
@@ -130,6 +127,34 @@
                     </div>
                     <div class="player-item">
                         <img src="/asset/img/fifa/psg.png" alt="">
+                    </div>
+                    <div class="line-item"></div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-4 mb-5">
+                <div class="card-player">
+                    <div class="pseudo">
+                        <h3 class="font h4 thrill-white">RAS</h3>
+                    </div>
+                    <div class="name">
+                        <p class="thrill-gray">Perbal Julien</p>
+                    </div>
+                    <div class="role">
+                        <div class="line-left"></div>
+                        <h4 class="font h6">joueur</h4>
+                        <div class="line-bottom"></div>
+                    </div>
+                    <div class="social">
+                        <a href="https://twitter.com/julien_Ras45">
+                            <img src="/asset/img/icon/Twitter/white.png" alt="icon twitter">
+                        </a>
+                        <a href="https://www.twitch.tv/julien_ras/">
+                            <img src="/asset/img/icon/Twitch/white.png" alt="icon Twitch">
+                        </a>
+                    </div>
+                    <div class="player-item">
+                        <img src="/asset/img/fifa/arsenal.png" alt="">
                     </div>
                     <div class="line-item"></div>
                 </div>
