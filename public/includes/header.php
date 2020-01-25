59eceef1ec3bea8f8ecc10dd53089591434a2e85
David Teixeira <david.teixeira31@gmail.com>
2020-01-25 14:51:35 +0100

    update favicon

--- public/includes/header.php
+++ public/includes/header.php
@@ -16,14 +16,27 @@
         <meta property="og:url" content="https://thrillesportclub.fr/" />
         <meta property="og:site_name" content="Thrill Esport Club" />
         <meta property="og:image" content="https://thrillesportclub.fr/asset/img/logo/v3/thrill-esport-logo.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:description" content="Thrill eSport Club est une structure esportive sous loi 1901, sur les jeux : FIFA, COD, FORTNITE.." />
-        <meta name="twitter:title" content="Thrill Esport Club" />
-        <meta name="twitter:site" content="@Thrill_EC" />
-        <meta name="twitter:creator" content="@Thrill_EC" />
     <?php endif; ?>
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:description" content="Thrill eSport Club est une structure esportive sous loi 1901, sur les jeux : FIFA, COD, FORTNITE.." />
+    <meta name="twitter:title" content="Thrill Esport Club" />
+    <meta name="twitter:site" content="@Thrill_EC" />
+    <meta name="twitter:creator" content="@Thrill_EC" />
 
-    <link rel="icon" type="image/png" href="/asset/img/logo/v3/thrill-esport-logo.png" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/asset/img/favicon/favicon.png" type="image/png">
+    <link rel="icon" sizes="32x32" href="/asset/img/favicon/favicon-32.png" type="image/png">
+    <link rel="icon" sizes="64x64" href="/asset/img/favicon/favicon-64.png" type="image/png">
+    <link rel="icon" sizes="96x96" href="/asset/img/favicon/favicon-96.png" type="image/png">
+    <link rel="icon" sizes="196x196" href="/asset/img/favicon/favicon-196.png" type="image/png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/asset/img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/asset/img/favicon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/asset/img/favicon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/asset/img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/asset/img/favicon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/asset/img/favicon/apple-touch-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="/asset/img/favicon/favicon-144.png">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -51,6 +64,3 @@
         <?php require_once("nav.php") ?>
     </header>
     <main>
-
-        <!-- /* --------------------------------- logger --------------------------------- */ -->
-        <!-- <?= var_dump($_SERVER) ?> -->
